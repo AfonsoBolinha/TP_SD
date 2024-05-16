e2c028b7d53b23b985b0397366fd87122bc3f7f3
Afonso Martins <afonsomartinspaivamiguel@gmail.com>
2024-05-16 16:32:10 +0100

    1st Pull

---
+++ src/main/java/com/example/tp_sd/TpSdApplication.java
@@ -0,0 +1,16 @@
+package com.example.tp_sd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpSdApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(TpSdApplication.class, args);
+
+
+    }
+
+}
