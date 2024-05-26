46d91827add56aaf444d47ea8d185f2e0a08ba04
S-Sphere <s0.miguel.santos@gmail.com>
2024-05-26 14:20:27 +0100

    Encontrar por contacto. para o login

--- src/main/java/com/example/tp_sd/Tabelas/AlunoInterface.java
+++ src/main/java/com/example/tp_sd/Tabelas/AlunoInterface.java
@@ -2,4 +2,5 @@ package com.example.tp_sd.Tabelas;
 import org.springframework.data.repository.CrudRepository;
 
 public interface AlunoInterface extends CrudRepository<AlunoEntity, Integer>{
+    AlunoEntity findByContacto(String contacto);
 }
