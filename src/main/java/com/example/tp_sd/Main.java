6074d7294db3efe83cbd254013ab5b7edcc8cd08
S-Sphere <s0.miguel.santos@gmail.com>
2024-05-31 23:47:58 +0100

    Modificações na página do admin. add registar/remover, mostrar e atualizar - alunos - professores - cursos
    add mostrar
    - administradores

--- src/main/java/com/example/tp_sd/Main.java
+++ src/main/java/com/example/tp_sd/Main.java
@@ -11,7 +11,10 @@ import org.springframework.web.bind.annotation.*;
 import java.sql.Timestamp;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.Optional;
 
 
 @Controller // This means that this class is a Controller
@@ -115,23 +118,49 @@ public class Main {
         return "Estatisticas/DeOndeVemOsNossosAlunos";
     }
 
+    // ============================================= Alunos ============================================================
+    @GetMapping(path="/showAlunos")
+    public String showAlunos(Model model) {
+        List<AlunoEntity> alunos = (List<AlunoEntity>) alunoInterface.findByStatus(1);
+        model.addAttribute("alunos", alunos);
+        return "Show/showAlunos";
+    }
     @GetMapping(path="/InscreverAluno")
     public String inscreverAluno(Model model) {
+        if (userStatus==3) {
             model.addAttribute("aluno", new AlunoEntity());
             return "Inscricao/InscreverAluno";
+        }
+        else {
+            return"redirect:/";
+        }
     }
     @PostMapping(path="/InscreverAlunoPost")
-    public String inscreverAluno(@RequestParam String nome,
-                                             @RequestParam String contacto,
-                                             @RequestParam String password,
-                                             @RequestParam String dataNascimento,
-                                             @RequestParam String proviniência) {
+    public String inscreverOuAtualizarAluno(@RequestParam(required = false) Integer id,
+                                                @RequestParam String nome,
+                                                @RequestParam String contacto,
+                                                @RequestParam(required = false) String password,
+                                                @RequestParam String dataNascimento,
+                                                @RequestParam String proviniência) {
 
-        AlunoEntity aluno = new AlunoEntity();
+        AlunoEntity aluno;
+        if (id != null) {
+            // Atualizar professor existente
+            Optional<AlunoEntity> optionalAluno = alunoInterface.findById(id);
+            if (optionalAluno.isPresent()) {
+                aluno = optionalAluno.get();
+                optionalAluno.get().setPassword(encoder.encode(password));
+            } else {
+                return "redirect:/showAlunos";
+            }
+        } else {
+            // Criar novo aluno
+            aluno = new AlunoEntity();
+            String encodedPassword = encoder.encode(password);
+            aluno.setPassword(encodedPassword);
+        }
         aluno.setNome(nome);
         aluno.setContacto(contacto);
-        String encodedPassword = encoder.encode(password);
-        aluno.setPassword(encodedPassword);
 
         // Conversão de dataNascimento de String para Timestamp
         try {
@@ -142,13 +171,51 @@ public class Main {
         } catch (ParseException e) {
             e.printStackTrace(); // Trate o erro conforme necessário
         }
-
         aluno.setProviniência(proviniência);
         aluno.setStatus(1);
         alunoInterface.save(aluno);
-        return "redirect:/homeAdmin";
+
+        return "redirect:/showAlunos";
     }
 
+    @GetMapping("/deleteAluno/{id}")
+    public String deleteAluno(@PathVariable(value = "id") Integer id ) {
+        alunoInterface.deleteById(id);
+        return "redirect:/showAlunos";
+    }
+
+    @GetMapping("/showUpdateAlunoForm{id}")
+    public String showUpdateAlunoForm(@PathVariable(value = "id") int id, Model model) {
+        Optional<AlunoEntity> optional = alunoInterface.findById(id);
+        AlunoEntity aluno = null;
+        if(optional.isPresent()){
+            aluno = optional.get();
+        }
+        else {
+            throw new RuntimeException("Aluno not found for id :: " + id);
+        }
+        model.addAttribute("aluno", aluno);
+        return "Show/updateAluno";
+    }
+
+
+    // ======================================== Cursos =================================================================
+    @GetMapping(path="/showCursos")
+    public String showCursos(Model model) {
+        List<String> nomesProfessores = new ArrayList<>();
+        List<CursoEntity> cursos = (List<CursoEntity>) cursoInterface.findAll();
+        for (CursoEntity curso : cursos) {
+            Optional<AlunoEntity> optionalProfessor = alunoInterface.findById(curso.getIdProfessor());
+            if (optionalProfessor.isPresent()) {
+                nomesProfessores.add(optionalProfessor.get().getNome());
+            } else {
+                nomesProfessores.add("Nome não encontrado");
+            }
+        }
+        model.addAttribute("nomesProfessores", nomesProfessores);
+        model.addAttribute("cursos", cursos);
+        return "Show/showCursos";
+    }
     @GetMapping(path="/InscreverCurso")
     public String inscreverCurso(Model model) {
         if (userStatus==3){
@@ -161,27 +228,63 @@ public class Main {
         }
     }
     @PostMapping(path="/InscreverCursoPost")
-    public String inscreverCurso(@RequestParam String nome,
-                                               @RequestParam Integer ano,
-                                               @RequestParam Integer idProfessor,
-                                               @RequestParam Integer nHoras) {
+    public String inscreverOuAtualizarCurso(@RequestParam(required = false) Integer id,
+                                            @RequestParam String nome,
+                                            @RequestParam Integer ano,
+                                            @RequestParam Integer idProfessor,
+                                            @RequestParam Integer nHoras) {
 
-        CursoEntity curso = new CursoEntity();
+        CursoEntity curso;
+        if (id != null) {
+            // Atualizar curso existente
+            Optional<CursoEntity> optionalCurso = cursoInterface.findById(id);
+            if (optionalCurso.isPresent()) {
+                curso = optionalCurso.get();
+            } else {
+                return "redirect:/showCursos";
+            }
+        } else {
+            // Criar novo curso
+            curso = new CursoEntity();
+        }
         curso.setNome(nome);
         curso.setAno(ano);
         curso.setIdProfessor(idProfessor);
         curso.setnHoras(nHoras);
         cursoInterface.save(curso);
 
-        // Obter o ID do curso recém-criado
-        Integer idCurso = curso.getId();
-        ParticipanteEntity participante = new ParticipanteEntity();
-        participante.setIdCurso(idCurso);
-        participante.setIdAluno(idProfessor);
-        participanteInterface.save(participante);
-        return "redirect:/adminHome";
+        return "redirect:/showCursos";
     }
 
+    @GetMapping("/deleteCurso/{id}")
+    public String deleteCurso(@PathVariable(value = "id") Integer id ) {
+        cursoInterface.deleteById(id);
+        return "redirect:/showCursos";
+    }
+
+    @GetMapping("/showUpdateCursoForm{id}")
+    public String showUpdateCursoForm(@PathVariable(value = "id") int id, Model model) {
+        Optional<CursoEntity> optional = cursoInterface.findById(id);
+        CursoEntity curso = null;
+        if(optional.isPresent()){
+            curso = optional.get();
+        }
+        else {
+            throw new RuntimeException("Curso not found for id :: " + id);
+        }
+        List<AlunoEntity> professors = alunoInterface.findByStatus(2);
+        model.addAttribute("professores", professors);
+        model.addAttribute("curso", curso);
+        return "Show/updateCurso";
+    }
+
+    // ======================================== Professores ============================================================
+    @GetMapping(path="/showProfessores")
+    public String showProfessores(Model model) {
+        List<AlunoEntity> professores = (List<AlunoEntity>) alunoInterface.findByStatus(2);
+        model.addAttribute("professores", professores);
+        return "Show/showProfessores";
+    }
     @GetMapping(path="/InscreverProfessor")
     public String inscreverProfessor(Model model) {
         if (userStatus==3){
@@ -193,17 +296,31 @@ public class Main {
         }
     }
     @PostMapping(path="/InscreverProfessorPost")
-    public String inscreverProfessor(@RequestParam String nome,
-                                               @RequestParam String contacto,
-                                               @RequestParam String password,
-                                               @RequestParam String dataNascimento,
-                                               @RequestParam String proviniência) {
+    public String inscreverOuAtualizarProfessor(@RequestParam(required = false) Integer id,
+                                                @RequestParam String nome,
+                                                @RequestParam String contacto,
+                                                @RequestParam(required = false) String password,
+                                                @RequestParam String dataNascimento,
+                                                @RequestParam String proviniência) {
 
-        AlunoEntity professor = new AlunoEntity();
+        AlunoEntity professor;
+        if (id != null) {
+            // Atualizar professor existente
+            Optional<AlunoEntity> optionalProfessor = alunoInterface.findById(id);
+            if (optionalProfessor.isPresent()) {
+                professor = optionalProfessor.get();
+                optionalProfessor.get().setPassword(encoder.encode(password));
+            } else {
+                return "redirect:/showProfessores";
+            }
+        } else {
+            // Criar novo professor
+            professor = new AlunoEntity();
+            String encodedPassword = encoder.encode(password);
+            professor.setPassword(encodedPassword);
+        }
         professor.setNome(nome);
         professor.setContacto(contacto);
-        String encodedPassword = encoder.encode(password);
-        professor.setPassword(encodedPassword);
         // Conversão de dataNascimento de String para Timestamp
         try {
             SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd"); // Formato para data sem hora
@@ -213,13 +330,43 @@ public class Main {
         } catch (ParseException e) {
             e.printStackTrace(); // Trate o erro conforme necessário
         }
-
         professor.setProviniência(proviniência);
         professor.setStatus(2);
         alunoInterface.save(professor);
-        return "redirect:/homeAdmin";
+
+        return "redirect:/showProfessores";
     }
 
+    @GetMapping("/deleteProf/{id}")
+    public String deleteProf(@PathVariable(value = "id") Integer id ) {
+        alunoInterface.deleteById(id);
+        return "redirect:/showProfessores";
+    }
+
+    @GetMapping("/showUpdateProfessorForm{id}")
+    public String showUpdateProfessorForm(@PathVariable(value = "id") int id, Model model) {
+        Optional<AlunoEntity> optional = alunoInterface.findById(id);
+        AlunoEntity professor = null;
+        if(optional.isPresent()){
+            professor = optional.get();
+        }
+        else {
+            throw new RuntimeException("Professor not found for id :: " + id);
+        }
+        model.addAttribute("professor", professor);
+        return "Show/updateProfessor";
+    }
+
+    // ============================================= Alunos ============================================================
+    @GetMapping(path="/showAdmins")
+    public String showAdmins(Model model) {
+        List<AlunoEntity> admins = (List<AlunoEntity>) alunoInterface.findByStatus(3);
+        model.addAttribute("admins", admins);
+        return "Show/showAdmins";
+    }
+
+
+    // =================================================================================================================
     @GetMapping(path="/MinhasTurmas")
     public String minhasTurmas(Model model) {
         if (userStatus == 2) {
--- src/main/java/com/example/tp_sd/Tabelas/AlunoInterface.java
+++ src/main/java/com/example/tp_sd/Tabelas/AlunoInterface.java
@@ -1,6 +1,11 @@
 package com.example.tp_sd.Tabelas;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
 
 public interface AlunoInterface extends CrudRepository<AlunoEntity, Integer>{
     AlunoEntity findByContacto(String contacto);
+    //@Query("SELECT a FROM AlunoEntity a WHERE a.status = 2")
+    List<AlunoEntity> findByStatus(Integer status);
 }
